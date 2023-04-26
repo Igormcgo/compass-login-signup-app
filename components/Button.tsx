0e8d5b862abd676aa368f49262d035726c459b5f
Igor Machado <igor.oliveira.pb@compasso.com.br>
2023-04-26 10:14:29 -0300

    Only for organization

---
+++ components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Colors } from '../constants/styles';
+import { View, StyleSheet, Pressable } from 'react-native';
+
+type ButtonProps = {}
+
+function Button () : JSX.Element {
+    return(
+        <View> teste </View>
+    );
+}
+
+export default Button;
